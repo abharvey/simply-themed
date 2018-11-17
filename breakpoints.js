8cc3bbd743040ceda109a1d03f0e826bfe3df55f
Alex Harvey <harv@Grimlock.localdomain>
2018-11-17 17:48:04 -0500

    created basic breakpoint functions for themed media queries

---
+++ breakpoints.js
@@ -0,0 +1,54 @@
+import { css } from 'styled-components';
+
+/**
+ * usage:
+ * 
+ * const breaks = {
+ *    small: { max: 1024 },
+ *    medium: { min: 1023, max: 1366 },
+ *    large: { min: 1367 }
+ *};
+ * 
+ * const mediaQueries = mediaQueryMixin(breaks);
+ * 
+ * const StyledDiv = styled.div`
+ *      ${mediaQueries.small`
+ *          width: 100%;
+ *      `}
+ *      ${mediaQueries.medium`
+ *          width: 75%;
+ *      `}
+ * `;
+ * 
+ */
+
+// TODO: Work on the naming
+
+const buildQuery = ({ min, max }) => {
+    if (min && max) {
+        return `@media all and (max-width: ${max}px) and (min-width: ${min}px)`
+    }
+
+    if (max) {
+        return `@media (max-width: ${max}px)`;
+    }
+
+    if (min) {
+        return `@media (min-width: ${min}px)`;
+    }
+
+    return '';
+};
+
+const buildQueryFunction = ({ min, max }) => (...args) => css`
+    ${buildQuery({ min, max })} {
+        ${css(...args)}
+    }
+`;
+
+const mediaQueryMixin = (breakpointObject) => Object.keys(breakpointObject).reduce((mixin, bp) => {
+    mixin[bp] = buildQueryFunction(breakpointObject[bp]);
+    return mixin;
+}, {});
+
+export default mediaQueryMixin;
---
+++ breakpoints.test.js
@@ -0,0 +1,5 @@
+const breaks = {
+    small: { max: 1024 },
+    medium: { min: 1023, max: 1366 },
+    large: { min: 1367 }
+};
