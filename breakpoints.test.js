6371209ad16119adc5778d8495f17a95f6bb564f
Alex Harvey <alex.harvey@unb.ca>
2018-11-18 11:44:40 -0500

    init tests to TDD the imrpovements

--- breakpoints.test.js
+++ breakpoints.test.js
@@ -1,5 +1,15 @@
+import mediaQueryMixin from './breakpoints';
+
 const breaks = {
     small: { max: 1024 },
     medium: { min: 1023, max: 1366 },
     large: { min: 1367 }
 };
+
+describe('mediaQueryMixin', () => {
+    it('generates a function for each breakpoint object', () => {
+        const bps = mediaQueryMixin(breaks);
+
+        expect(typeof bps.small).toBe('function');
+    })
+})
